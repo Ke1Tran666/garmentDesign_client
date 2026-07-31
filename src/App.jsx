e3b6aa737e74d7e46b1832a05809f7e33ae89540
Ke1Tran666 <ke1tran666@gmail.com>
2026-07-31 14:53:00 +0700

    Update the route.

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,50 @@
-import './App.css'
-import { Navigate, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import { NotificationProvider } from './components/ui/Notification/NotificationContext';
-import LoginPage from './pages/Auth/LoginPage';
-import RegisterPage from './pages/Auth/RegisterPage';
-import ForgotPasswordPage from './pages/Auth/ForgotPasswordPage';
-import UserLayout from './layouts/UserLayout/UserLayout';
-import AuthLayout from './layouts/AuthLayout/AuthLayout';
-import ProfilePage from './pages/User/Account/ProfilePage';
-import AddressPage from './pages/User/Account/AddressPage';
-import SecurityPage from './pages/User/Account/SecurityPage';
-import DashboardPage from './pages/User/Main/DashboardPage';
-import PrivacyPage from './pages/User/Account/PrivacyPage';
-import ServiceOrderPage from './pages/User/Services/ServiceOrderPage';
+import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import LoginPage from "./pages/Auth/LoginPage";
+import RegisterPage from "./pages/Auth/RegisterPage";
+import ForgotPasswordPage from "./pages/Auth/ForgotPasswordPage";
+
+import UserLayout from "./layouts/UserLayout/UserLayout";
+import AuthLayout from "./layouts/AuthLayout/AuthLayout";
+
+import DashboardPage from "./pages/User/Main/DashboardPage";
+import ServiceOrderPage from "./pages/User/Services/ServiceOrderPage";
+import ProfilePage from "./pages/User/Account/ProfilePage";
+import AddressPage from "./pages/User/Account/AddressPage";
+import SecurityPage from "./pages/User/Account/SecurityPage";
+import PrivacyPage from "./pages/User/Account/PrivacyPage";
+
+import { NotificationProvider } from "./components/ui/Notification/NotificationContext";
 
 const App = () => {
-
   return (
     <NotificationProvider>
       <Routes>
         <Route path="/" element={<Home />}/>
 
-        {/* AUTH */}
         <Route element={<AuthLayout />}>
-
-          {/* LOGIN */}
-          <Route path="/login" element={<LoginPage />} />
-
-          {/* REGISTER */}
-          <Route path="/register" element={<RegisterPage />} />
-          
-          {/* FORGOT PASSWORD */}
-          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          <Route path="/login" element={<LoginPage />}/>
+          <Route path="/register" element={<RegisterPage />}/>
+          <Route path="/forgot-password" element={<ForgotPasswordPage />}/>
         </Route>
 
-        {/* MAIN USER */}
         <Route path="/user" element={<UserLayout />}>
-          <Route index element={<Navigate to="dashboard" replace />} />
-          <Route path="dashboard" element={<DashboardPage />} />
-        </Route>
+          <Route index element={<Navigate to="profile" replace/>}/>
 
-        {/* SERVICE USER */}
-        <Route path="/user" element={<UserLayout />}>
-          <Route index element={<Navigate to="service-order" replace />} />
-          <Route path="service-order" element={<ServiceOrderPage />} />
-        </Route>
+          <Route path="dashboard" element={<DashboardPage />}/>
 
-        {/* USER */}
-        <Route path="/user" element={<UserLayout />}>
-          <Route index element={<Navigate to="profile" replace />} />
-          <Route path="profile" element={<ProfilePage />} />
-          <Route path="address" element={<AddressPage />} />
-          <Route path="security" element={<SecurityPage />} />
-          <Route path="privacy" element={<PrivacyPage />} />
+          <Route path="service-order" element={<ServiceOrderPage />}/>
+
+          <Route path="profile" element={<ProfilePage />}/>
+          <Route path="address" element={<AddressPage />}/>
+          <Route path="security" element={<SecurityPage />}/>
+          <Route path="privacy" element={<PrivacyPage />}/>
         </Route>
       </Routes>
     </NotificationProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;

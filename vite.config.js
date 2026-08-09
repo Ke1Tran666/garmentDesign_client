2d1a6dc2996f86c24be10483201edfd42d582c43
Ke1Tran666 <ke1tran666@gmail.com>
2026-08-09 13:25:05 +0700

    update vite config.

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import {fileURLToPath, URL} from "node:url";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@": fileURLToPath(
+        new URL("./src", import.meta.url),
+      ),
     },
   },
+
   test: {
     environment: "jsdom",
     setupFiles: "./src/test/setup.js",
   },
+
   server: {
     host: true,
     allowedHosts: true,
+
+    headers: {
+      "Cross-Origin-Opener-Policy":
+        "same-origin-allow-popups",
+    },
   },
-})
+});
